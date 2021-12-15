d8a7bc0a218fb7ae1004e339ed0d9ed3113f05c4
Marcel Tomczak <7468254-MarcelTomczak996@users.noreply.gitlab.com>
2021-12-15 23:14:56 +0100

    resolve conflicts

--- src/main/java/com/example/web_restauracje/controller/RestaurantController.java
+++ src/main/java/com/example/web_restauracje/controller/RestaurantController.java
@@ -2,12 +2,15 @@ package com.example.web_restauracje.controller;
 
 import com.example.web_restauracje.models.Database;
 import com.example.web_restauracje.service.RestaurantService;
+import com.google.firebase.FirebaseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.ExecutionException;
 
 @Controller
 @RequestMapping("/restaurants")
